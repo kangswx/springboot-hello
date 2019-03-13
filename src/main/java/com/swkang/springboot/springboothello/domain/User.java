f0d6abce550859f38a48ec0636da4b9d5e160072
kangswx <996334837@qq.com>
2019-03-13 22:57:59 +0800

    添加json注解

--- src/main/java/com/swkang/springboot/springboothello/domain/User.java
+++ src/main/java/com/swkang/springboot/springboothello/domain/User.java
@@ -1,10 +1,38 @@
 package com.swkang.springboot.springboothello.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
 public class User {
 
+
     private int age;
+    @JsonIgnore//不会序列化
     private String pwd;
+    @JsonProperty("account") //添加别名
+    @JsonInclude(JsonInclude.Include.NON_NULL) //为空的话不会反回
     private String phone;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", locale = "zh", timezone = "GMT+8") //格式化日期
+    private Date createTime;
+
+    public User(int age, String pwd, String phone, Date createTime) {
+        this.age = age;
+        this.pwd = pwd;
+        this.phone = phone;
+        this.createTime = createTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 
     public int getAge() {
         return age;
