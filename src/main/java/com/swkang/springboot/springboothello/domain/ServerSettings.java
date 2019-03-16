046f46117a901308a31eabf586bb82cfd066ba13
kangswx <996334837@qq.com>
2019-03-16 22:05:08 +0800

    新增配置文件的测试实体类

---
+++ src/main/java/com/swkang/springboot/springboothello/domain/ServerSettings.java
@@ -0,0 +1,36 @@
+package com.swkang.springboot.springboothello.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource({"classpath:application.properties"})
+@ConfigurationProperties
+public class ServerSettings {
+
+    //名称
+    @Value("${test.name}")
+    private String name;
+
+    //域名地址
+    @Value("${test.domain}")
+    private String domain;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+}
