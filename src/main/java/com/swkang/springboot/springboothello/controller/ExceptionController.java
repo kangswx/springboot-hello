63f347a0ce7c886853de9579a65e05e80ce8e2b1
kangswx <996334837@qq.com>
2019-03-16 23:47:13 +0800

    新增自定义异常测试方法

--- src/main/java/com/swkang/springboot/springboothello/controller/ExceptionController.java
+++ src/main/java/com/swkang/springboot/springboothello/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.swkang.springboot.springboothello.controller;
 
+import com.swkang.springboot.springboothello.domain.MyException;
 import com.swkang.springboot.springboothello.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class ExceptionController {
     public Object index(){
         int i = 1 / 0;
         return new User(11, "123212", "18659595959", new Date());
+    }
+
+    @RequestMapping("/api/v1/my_except")
+    public Object myExcept(){
+        throw new MyException("500", "my except异常");
     }
 }
