415aa735ae9af76ebf82605d554588a9d2b3dfab
kangswx <996334837@qq.com>
2019-03-16 22:19:05 +0800

    新增通过前缀的方式将配置文件中的参数注入到实体类里面

--- src/main/java/com/swkang/springboot/springboothello/domain/ServerSettings.java
+++ src/main/java/com/swkang/springboot/springboothello/domain/ServerSettings.java
@@ -1,21 +1,21 @@
 package com.swkang.springboot.springboothello.domain;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource({"classpath:application.properties"})
-@ConfigurationProperties
+//@ConfigurationProperties
+@ConfigurationProperties(prefix = "test")
 public class ServerSettings {
 
     //名称
-    @Value("${test.name}")
+    //@Value("${name}")  //使用前缀的话就不用通过Value的方法注入参数，不使用前缀的话需要通过Value的方式注入配置文件中的参数
     private String name;
 
     //域名地址
-    @Value("${test.domain}")
+    //@Value("${domain}")
     private String domain;
 
     public String getName() {
