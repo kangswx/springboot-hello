44d074f216c493554b90541a5969013eb5ee726f
kangswx <996334837@qq.com>
2019-03-16 23:24:48 +0800

    新增全局异常处理类

---
+++ src/main/java/com/swkang/springboot/springboothello/domain/CustomExHandler.java
@@ -0,0 +1,29 @@
+package com.swkang.springboot.springboothello.domain;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExHandler {
+
+    /**
+     * 捕获全局异常，处理所有不可知的异常
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    Object handlerException(Exception e, HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 100);
+        map.put("msg", e.getMessage());
+        map.put("url", request.getRequestURL());
+        return map;
+    }
+}
