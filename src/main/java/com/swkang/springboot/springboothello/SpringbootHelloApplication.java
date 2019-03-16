e5392c380309baef512ce6ed992920a80fb148a1
kangswx <996334837@qq.com>
2019-03-16 15:28:30 +0800

    新增对上传文件大小的限制

--- src/main/java/com/swkang/springboot/springboothello/SpringbootHelloApplication.java
+++ src/main/java/com/swkang/springboot/springboothello/SpringbootHelloApplication.java
@@ -2,11 +2,26 @@ package com.swkang.springboot.springboothello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class SpringbootHelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHelloApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+
+        //单个文件最大
+        factory.setMaxFileSize("10240KB");
+        //设置上传数据总大小
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
     }
 }
 
