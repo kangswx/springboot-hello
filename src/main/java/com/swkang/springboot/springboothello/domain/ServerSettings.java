1d6f2c7eccb86f5351aed13a18bf0e5b3e8a9584
kangswx <996334837@qq.com>
2019-03-18 08:42:21 +0800

    代码优化

--- src/main/java/com/swkang/springboot/springboothello/domain/ServerSettings.java
+++ src/main/java/com/swkang/springboot/springboothello/domain/ServerSettings.java
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Component;
 public class ServerSettings {
 
     //名称
-    //@Value("${name}")  //使用前缀的话就不用通过Value的方法注入参数，不使用前缀的话需要通过Value的方式注入配置文件中的参数
+    //@Value("${test.name}")  //使用前缀的话就不用通过Value的方法注入参数，不使用前缀的话需要通过Value的方式注入配置文件中的参数
     private String name;
 
     //域名地址
-    //@Value("${domain}")
+    //@Value("${test.domain}")
     private String domain;
 
     public String getName() {
