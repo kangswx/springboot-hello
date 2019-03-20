e5f51e6c70e2f8625b3d8dac7a4e42a7947110bd
kangswx <996334837@qq.com>
2019-03-20 23:41:40 +0800

    新增UsersService

---
+++ src/main/java/com/swkang/springboot/springboothello/service/UsersService.java
@@ -0,0 +1,8 @@
+package com.swkang.springboot.springboothello.service;
+
+import com.swkang.springboot.springboothello.domain.Users;
+
+public interface UsersService {
+
+    public int add(Users user);
+}
