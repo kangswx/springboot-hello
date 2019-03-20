1d8d32aa6eef3c38bf3de88644da27788c6e60e7
kangswx <996334837@qq.com>
2019-03-21 00:12:42 +0800

    新增crud方法

--- src/main/java/com/swkang/springboot/springboothello/mapper/UsersMapper.java
+++ src/main/java/com/swkang/springboot/springboothello/mapper/UsersMapper.java
@@ -1,8 +1,9 @@
 package com.swkang.springboot.springboothello.mapper;
 
 import com.swkang.springboot.springboothello.domain.Users;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * 访问数据库的接口
@@ -13,5 +14,40 @@ public interface UsersMapper {
     @Insert("insert into user(name, phone, create_date, age) values (#{name}, #{phone}, #{create_date}, #{age})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")  //使用数据库自增长字段
     int insert(Users user);
+
+    /**
+     * 查询所有的对象
+     * @return
+     */
+    @Select("select * from user")
+    @Results({
+            @Result(column = "create_date", property = "create_date")
+    })
+    List<Users> findAll();
+
+    /**
+     * 更具id查询对象
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    @Results({
+            @Result(column = "create_date", property = "create_date")
+    })
+    Users findById(Integer id);
+
+    /**
+     * 更新对象
+     * @param users
+     */
+    @Update("update user set name = #{name} where id = #{id}")
+    void update(Users users);
+
+    /**
+     * 删除对象
+     * @param id
+     */
+    @Delete("delete from user where id = #{id}")
+    void delete(Integer id);
 
 }
