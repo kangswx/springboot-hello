538e2afbd58e79b9c9320a29a652e10819c425e1
kangswx <996334837@qq.com>
2019-03-21 23:07:42 +0800

    增加事务的相关方法

--- src/main/java/com/swkang/springboot/springboothello/service/impl/UsersServiceImpl.java
+++ src/main/java/com/swkang/springboot/springboothello/service/impl/UsersServiceImpl.java
@@ -5,8 +5,13 @@ import com.swkang.springboot.springboothello.mapper.UsersMapper;
 import com.swkang.springboot.springboothello.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
+@Transactional
 public class UsersServiceImpl implements UsersService {
 
     @Autowired
@@ -17,5 +22,18 @@ public class UsersServiceImpl implements UsersService {
         usersMapper.insert(user);
         int id = user.getId();
         return id;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int addAccount() {
+        Users users = new Users();
+        users.setAge(88);
+        users.setCreate_date(new Date());
+        users.setName("测试事务啦");
+        users.setPhone("18645757489");
+        usersMapper.insert(users);
+        int i = 9 / 0;
+        return 0;
     }
 }
